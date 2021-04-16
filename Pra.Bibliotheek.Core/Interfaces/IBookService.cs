a8850ed9c582ebb32d9f4f58cea90a4c6f7efb35
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-04-16 16:01:37 +0200

    minor issues resolved

--- Pra.Bibliotheek.Core/Interfaces/IBookService.cs
+++ Pra.Bibliotheek.Core/Interfaces/IBookService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Pra.Bibliotheek.Core.Entities;
 
 namespace Pra.Bibliotheek.Core.Interfaces
